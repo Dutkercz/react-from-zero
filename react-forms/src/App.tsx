a0b969379f291c1a1d764729e7234b43c8d334b5
Dutkercz <dutkercz@gmail.com>
2025-12-28 12:46:13 -0300

    Finalizando a seção de forms

--- react-forms/src/App.tsx
+++ react-forms/src/App.tsx
@@ -1,13 +1,21 @@
-import './App.css'
-import MyForm from './components/MyForm'
+import "./App.css";
+import MyForm from "./components/MyForm";
+import type { User } from "./components/User";
 
 function App() {
+  const user: User = {
+    name: "Cristain",
+    email: "dutkercz@gmail.com",
+    bio: "Minha biografia de usuario",
+    role: "admin",
+  };
+
   return (
     <>
-     <h2 style={{textAlign : "center"}}>Forms</h2>
-     <MyForm/>
+      <h2 style={{ textAlign: "center" }}>Forms</h2>
+      <MyForm userProps={user} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-forms/src/components/MyForm.tsx
+++ react-forms/src/components/MyForm.tsx
@@ -1,20 +1,43 @@
 import { useState, type FormEvent } from "react";
 import "./MyForm.css";
+import type { User } from "./User";
 
-const MyForm = () => {
+
+  interface MyFormProps {
+    userProps: User
+  }
+
+const MyForm = ({ userProps }: MyFormProps) => {
+
   // 3 - gerenciamento de dados
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
+  const [name, setName] = useState(userProps ? userProps.name : "");
+  const [email, setEmail] = useState(userProps ? userProps.email : "");
+  const [bio, setBio] = useState(userProps ? userProps.bio : "");
+  const [role, setRole] = useState(userProps ? userProps.role : "");
+  // se userProps estiver presente, preenchemos nosso form com informações desse userProps
 
   const handleName = (e: any) => {
     setName(e.target.value);
   };
 
+  const handleBio = (e : any) => {
+    setBio(e.target.value)
+  }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault() //impede o recarregamento da pagina
     console.log("Enviando o form")
-    console.log("nome " + name);
-    console.log("email " + email);
+    console.log(name, email, bio, role);
+    //validar
+    //enviar
+    //...e por fim.
+
+    // 7 - LIMPAR FORMULAR : util quando estamos em um form de cadastro em que não temos o redirect pra outra page
+    //ou seja, iremos repetir a operação de cadastro varias vezes, como o cadastro de produtos
+    setName("");
+    setEmail("");
+    setBio("")
+    setRole("")
   }
 
   console.log(name);
@@ -22,6 +45,7 @@ const MyForm = () => {
 
   return (
     // 5 - ENVIO DE FORM
+    // 6 - CONTROLED INPUTS
     <div>
       {/* 1- criação de Form*/}
       <form onSubmit={handleSubmit}>
@@ -30,6 +54,7 @@ const MyForm = () => {
           <input
             type="text"
             name="name"
+            value={name}
             placeholder="Seu nome"
             onChange={handleName}
           />
@@ -42,6 +67,7 @@ const MyForm = () => {
             <input
               type="email"
               name="email"
+              value={email} //caso o formulario ja venha com alguma informação dentro da props, pode utilizar direto no campo
               placeholder="seu melhor email"
               // 4 - SIMPLIFICANDO A FUNÇÃO DE MANIPULAR O STATE, USANDO O METODO INLINE
               onChange={(e: any) => {
@@ -49,6 +75,26 @@ const MyForm = () => {
               }}
             />
           </label>
+        </div>
+
+        {/* Text area: usando quando temos muitos caracteres como posts de blogs, bios, descrições, etc*/}
+        <div>
+              <label >
+                <span>Biografia</span>
+                <textarea name="bio" placeholder="descrição do usuario" onChange={handleBio} value={bio}></textarea>
+              </label>
+        </div>
+
+        {/* Select: caixinha tradicional com opções de seleção como para generos, papeis, select de cidade, etc*/}
+        <div>
+              <label>
+                <span>Função no sistema</span>
+                <select name="role" onChange={(e) => {setRole(e.target.value)}} value={role}>
+                  <option value="user">Usuario</option>
+                  <option value="editor">Editor</option>
+                  <option value="admin">Administrador</option>
+                </select>
+              </label>
         </div>
         <input type="submit" value="Enviar" />
       </form>
