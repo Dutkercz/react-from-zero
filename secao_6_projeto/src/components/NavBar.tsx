1abb4827f80307b93de9a740d515b00ac43bc1de
Dutkercz <dutkercz@gmail.com>
2026-01-16 15:30:29 -0300

    Substituindo o Link, pelo no NavLink em NavBar.tsx

--- secao_6_projeto/src/components/NavBar.tsx
+++ secao_6_projeto/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import "./NavBar.css"
 
 const NavBar = () => {
@@ -7,9 +7,15 @@ const NavBar = () => {
       {/* Aqui são responsaveis por mudar a URL. Ou seja, ao clicar em alguns do
       botoes abaixo, minha URL ira mudar. Consequentemente nosso Route no App.tsx irá captar
       a mudança e ira renderizar a respectiva Page */}
-        <Link to="/">Home</Link>
+
+        {/* Links antigos, antes do navLink
+        * <Link to="/">Home</Link>
         <Link to="/about">Sobre</Link>
-        <Link to="/products">Produtos</Link>
+        <Link to="/products">Produtos</Link> */}
+
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
+        <NavLink to="/products">Produtos</NavLink>
     </nav>
   )
 }
--- secao_6_projeto/src/pages/NotFound.tsx
+++ secao_6_projeto/src/pages/NotFound.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>
+        <h1>Erro 404</h1>
+        <p>Não há nada por aqui =]</p>
+    </div>
   )
 }
 
